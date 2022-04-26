4320eb57ff14dcd8c41bf28409c6bf8b878fb60d
Liu Ji <mynameisliuji@foxmail.com>
2022-04-26 20:46:33 +0800

    config

--- config/config.prod.example.js
+++ config/config.prod.example.js
@@ -31,7 +31,7 @@ module.exports = appInfo => {
           password: '123456',
           database: 'jianghujs_demo_basic'
         },
-        pool: { min: 10, max: 100 },
+        pool: { min: 0, max: 100 },
         acquireConnectionTimeout: 30000
       },
       app: true
